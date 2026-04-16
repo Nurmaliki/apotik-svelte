a739e0791144fd407767ae60e88ff3f47ee0f93e
Nur Maliki <34450979+Nurmaliki@users.noreply.github.com>
2026-04-16 09:56:07 +0700

    Create Navbar navigation component

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,27 @@
+<script>
+  export let title = 'Navbar';
+</script>
+
+<nav>
+  <ul>
+    <li><a href="/">Home</a></li>
+    <li><a href="/about">About</a></li>
+    <li><a href="/contact">Contact</a></li>
+  </ul>
+</nav>
+
+<style>
+  nav {
+    background-color: #333;
+    color: white;
+    padding: 1rem;
+  }
+  nav ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  nav ul li {
+    display: inline;
+    margin-right: 1rem;
+  }
+</style>
