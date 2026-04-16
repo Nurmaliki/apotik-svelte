6b4ccbf76a43fe7942f40183420dfb4904230d50
Nur Maliki <34450979+Nurmaliki@users.noreply.github.com>
2026-04-16 09:33:46 +0700

    Create vite.config.js with Svelte plugin configuration

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+'use strict';
+
+import { sveltekit } from '@sveltejs/kit/vite';
+
+/** @type {import('vite').UserConfig} */
+const config = {
+    plugins: [sveltekit()]
+};
+
+export default config;
